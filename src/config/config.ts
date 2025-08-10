ab7782f751f591cb0205ce92bdbc88dcec02f40a
Varsha Ijeri <varshaijeri28@gmail.com>
2025-08-09 19:45:56 -0500

    config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 // src/config.ts
-// const API_BASE_URL = "https://taskmanager-hidden-sky-719.fly.dev";
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = "https://taskmanager-hidden-sky-719.fly.dev";
+// const API_BASE_URL = "http://localhost:8080";
 export default API_BASE_URL;
